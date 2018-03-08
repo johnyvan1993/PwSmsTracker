70566889f1c5cddddfe989ca0a59d363b0bda71c
johnyvan1993 <johnyvan1993@gmail.com>
2018-03-08 10:32:31 +0700

    first_commit

---
+++ smstracker/src/main/java/com/library/pwsmstracker/smstracker/SharedPreferencesSmsStorage.java
@@ -0,0 +1,41 @@
+package com.library.pwsmstracker.smstracker;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+/**
+ * SmsStorage implementation based on shared preferences.
+ *
+ * @author luongvo
+ */
+class SharedPreferencesSmsStorage implements SmsStorage {
+
+    private static final String LAST_SMS_PARSED = "last_sms_parsed";
+    private static final int DEFAULT_SMS_PARSED_VALUE = -1;
+
+    private SharedPreferences preferences;
+
+    SharedPreferencesSmsStorage(SharedPreferences preferences) {
+        if (preferences == null) {
+            throw new IllegalArgumentException("SharedPreferences param can't be null");
+        }
+        this.preferences = preferences;
+    }
+
+    @Override
+    public void updateLastSmsIntercepted(int smsId) {
+        Editor editor = preferences.edit();
+        editor.putInt(LAST_SMS_PARSED, smsId);
+        editor.commit();
+    }
+
+    @Override
+    public int getLastSmsIntercepted() {
+        return preferences.getInt(LAST_SMS_PARSED, DEFAULT_SMS_PARSED_VALUE);
+    }
+
+    @Override
+    public boolean isFirstSmsIntercepted() {
+        return getLastSmsIntercepted() == DEFAULT_SMS_PARSED_VALUE;
+    }
+}
